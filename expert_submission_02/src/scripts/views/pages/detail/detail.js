7cdca65140b7c81864e33f432f5d62b500857bdf
LENOVO <bvkmobilereserv@gmal.com>
2023-05-23 14:06:16 +0700

    adding template for detail

--- expert_submission_02/src/scripts/views/pages/detail/detail.js
+++ expert_submission_02/src/scripts/views/pages/detail/detail.js
@@ -2,6 +2,7 @@ import './style.css';
 
 import UrlParser from '../../../routes/url-parser';
 import RestaurantDataSource from '../../../data/restaurant-source';
+import {placeholderDetailRestaurant} from '../../templates/placeholder-catalog';
 const Detail = {
   async render() {
     return `
@@ -9,46 +10,28 @@ const Detail = {
         <div class='placeholder-img shimmer'></div>
       </div>
       <div class='content-detail'>
-        <div class='restaurant-card'>
-          <div class='card-title'>
-            <div class='placeholder-title shimmer'></div>
-          </div>
-          <div class='card-body'>
-            <div class='placeholder-info shimmer'></div>
-            <div class='category-info'>
-              <div class='placeholder-category shimmer'></div>
-              <div class='placeholder-category shimmer'></div>
-              <div class='placeholder-category shimmer'></div>
-            </div>
-            <div class='descreption-info'>
-              <div class='placeholder-descreption full shimmer'></div>
-              <div class='placeholder-descreption quarter shimmer'></div>
-              <div class='placeholder-descreption half shimmer'></div>
-            </div>
-            <div class='card-footer'>
-              <div class='footer-info'>
-                <div class='placeholder-second shimmer'></div>
-                <div class='placeholder-300 shimmer'></div>
-               
-              </div>
-              <div class='footer-info'>
-                <div class='placeholder-second shimmer'></div>
-                <div class='placeholder-80 shimmer'></div>
-                <div class='placeholder-80 shimmer'></div>
-                <div class='placeholder-80 shimmer'></div>
-              </div>
-            </div>
-            
-          </div>
-        </div>
-      </div>
-    `;
+        <div class='restaurant-card'>${placeholderDetailRestaurant}</div>
+      </div>`;
   },
 
   async afterRender() {
+    const errorElement = document.querySelector('.error-msg');
+    const btnReload = document.querySelector('#btnreload');
+    btnReload.addEventListener('click', (event) => {
+      errorElement.style.display = 'none';
+      wrapperCatalog.innerHTML = placeholderItem;
+      this.getDetailRestaurant();
+    });
+    console.log(dataDetail);
+    const skipElelement = document.querySelector('.skip-link');
+    skipElelement.scrollIntoView(true);
+  },
+  async getDetailRestaurant() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const dataDetail = await RestaurantDataSource.getDetailRestaurant(url.id);
-    console.log(dataDetail);
+    if (!dataDetail.error) {
+
+    }
   },
 };
 
