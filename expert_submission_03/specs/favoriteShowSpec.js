e18eb747c39dd3539d071b56ff73382904d8a6d6
LENOVO <bvkmobilereserv@gmal.com>
2023-06-20 17:18:46 +0700

    testing showfavorite spec

--- expert_submission_03/specs/favoriteShowSpec.js
+++ expert_submission_03/specs/favoriteShowSpec.js
@@ -6,25 +6,25 @@ import FavoritePresenter
   from '../src/scripts/views/pages/favorite/favorite-presenter';
 
 
-// describe('Showing all favorite restaurant', () => {
-//   let view;
-//   const renderTemplate = () => {
-//     view = new FavoriteView();
-//     document.body.innerHTML = view.getTemplate();
-//   };
-//   beforeEach(() => {
-//     renderTemplate();
-//   });
+describe('Showing all favorite restaurant', () => {
+  let view;
+  const renderTemplate = () => {
+    view = new FavoriteView();
+    document.body.innerHTML = view.getTemplate();
+  };
+  beforeEach(() => {
+    renderTemplate();
+  });
 
-//   describe('When no restaurants have been liked', () => {
-//     it('should ask for the favorite restaurants', () => {
-//       const favoriteRestaurants = spyOnAllFunctions(FavoriteDatabase);
+  describe('When no restaurants have been liked', () => {
+    it('should ask for the favorite restaurants', () => {
+      const favoriteDatabase = spyOnAllFunctions(FavoriteDatabase);
+      new FavoritePresenter({view, favoriteRestaurant: favoriteDatabase});
+      expect(favoriteDatabase.getRestaurants).toHaveBeenCalledTimes(1);
+    });
 
-//       new FavoritePresenter({
-//         view,
-//         favoriteRestaurants,
-//       });
-//       expect(favoriteRestaurants.favoriteRestaurants).toHaveBeenCalledTimes(1);
-//     });
-//   });
-// });
+    it('should show the information that if empty', () => {
+      expect(document.querySelectorAll('#btnreload').length).toEqual(1);
+    });
+  });
+});
