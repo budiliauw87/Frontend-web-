965c72cf7e858867d0327a7bcce53aecebe372d6
LENOVO <bvkmobilereserv@gmal.com>
2023-06-22 11:25:31 +0700

    Adding removing favorite restaurant e2e

--- expert_submission_03/e2e/Liking_Restaurants.spec.js
+++ expert_submission_03/e2e/Liking_Restaurants.spec.js
@@ -9,9 +9,49 @@ Scenario('showing empty liked movies', ({I}) => {
   I.see('Favorite Empty !', '.title-error');
 });
 
-Scenario('Adding favorite restaurant', ({I}) => {
+Scenario('Adding favorite restaurant', async ({I}) => {
   I.see('Favorite Empty !', '.title-error');
   I.amOnPage('/');
-  pause();
+  I.seeElement('.catalog-item a');
+  const firstItem = locate('.catalog-item a').first();
+  const firstItemTitle = await I.grabTextFrom(firstItem);
+
+  I.click(firstItem);
+  I.seeElement('.address-wrapper h2');
+  I.seeElement('#btnfavorite');
+  I.click('#btnfavorite');
+
+  // go to favorite pages
+  I.amOnPage('/#/favorite');
+  I.seeElement('.catalog-item a');
+  const itemFavorite = locate('.catalog-item a').first();
+  const ItemFavoriteTitle = await I.grabTextFrom(itemFavorite);
+  assert.strictEqual(firstItemTitle, ItemFavoriteTitle);
+});
+
+Scenario('removing favorite restaurant', async ({I}) => {
+  // go to detail pages
+  I.see('Favorite Empty !', '.title-error');
+  I.amOnPage('/');
+  I.seeElement('.catalog-item a');
+  const firstItem = locate('.catalog-item a').first();
+
+  I.click(firstItem);
+  I.seeElement('.address-wrapper h2');
+  I.seeElement('#btnfavorite');
+  I.click('#btnfavorite');
+
+  // go to favorite pages
+  I.amOnPage('/#/favorite');
+  I.seeElement('.catalog-item a');
+  const itemFavorite = locate('.catalog-item a').first();
+  I.click(itemFavorite);
+
+  I.seeElement('.address-wrapper h2');
+  I.seeElement('#btnfavorite');
+  I.click('#btnfavorite');
+
+  I.amOnPage('/#/favorite');
+  I.see('Favorite Empty !', '.title-error');
 });
 
